99965f8d2003988203c8988334350b540af0d5fc
FlavSallMaix <fla.sm@orange.fr>
2025-04-23 07:48:50 +0200

    LanguageSwitch

--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -1,7 +1,15 @@
-export const ImageContainer = ( {src, alt} ) => {
-    return(
-        <>
-            <img src={src} alt={alt} className="size-full h-auto" />
-        </>
-    )
-}
+export const ImageContainer = ({ src, alt }) => {
+  return (
+    <>
+      <img src={src} alt={alt} className="size-full h-auto" />
+    </>
+  );
+};
+
+export const FlagContainer = ({ src, alt, code }) => {
+  return (
+    <>
+      <img src={src} alt={alt} className="" />
+    </>
+  );
+};
--- src/components/Switch.jsx
+++ src/components/Switch.jsx
@@ -1,17 +1,31 @@
 import { useTranslation } from "react-i18next";
 
 import "./components.css";
+import "./Container.jsx";
+import { FlagContainer } from "./Container.jsx";
 
 const LanguageSwitch = () => {
   const { i18n } = useTranslation();
 
-  const languageCode = i18n.language.split('-')[0]; 
+  const languageCode = i18n.language.split("-")[0];
   const currentLang = languageCode.toLowerCase();
 
   const languages = [
-    { code: "en", label: "English" },
-    { code: "ko", label: "한국어" },
-    { code: "fr", label: "Français" }
+    {
+      code: "en",
+      label: "English",
+      img: "src/assets/img/Flag_of_the_United_Kingdom_(3-5).svg.png",
+    },
+    {
+      code: "ko",
+      label: "한국어",
+      img: "src/assets/img/Flag_of_South_Korea.svg.png",
+    },
+    {
+      code: "fr",
+      label: "Français",
+      img: "src/assets/img/Flag_of_France.svg.png",
+    },
   ];
 
   // Sort them so the user’s current language is first
@@ -27,13 +41,34 @@ const LanguageSwitch = () => {
 
   return (
     <div id="language-switch">
+      <div id="mobile">
         <select onChange={changeLanguage} value={currentLang}>
-        {sortedLanguages.map((lang) => (
+          {sortedLanguages.map((lang) => (
             <option key={lang.code} value={lang.code}>
-            {lang.label}
+              {lang.img}
+              {lang.label}
             </option>
-        ))}
+          ))}
         </select>
+      </div>
+
+      <div id="desktop" class="flex flex-row gap-3">
+        <img
+          key="fr"
+          src="src\assets\img\Flag_of_France.svg.png"
+          alt="French flag"
+        ></img>
+        <img
+          key="en"
+          src="src\assets\img\Flag_of_the_United_Kingdom_(3-5).svg.png"
+          alt="English flag"
+        ></img>
+        <img
+          key="ko"
+          src="src\assets\img\Flag_of_South_Korea.svg.png"
+          alt="Korean flag"
+        ></img>
+      </div>
     </div>
   );
 };
