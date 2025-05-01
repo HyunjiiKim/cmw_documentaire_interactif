7d98d8e47abdfae2ae8b3da5488e3a6887b07e97
Hyunji Kim <hjkim96727@gmail.com>
2025-05-02 08:09:01 +0900

    implement loader

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,27 @@
-import { StrictMode } from "react";
+import { StrictMode, Suspense } from "react";
 import { createRoot } from "react-dom/client";
+import { useTranslation } from "react-i18next";
 
 import App from "./App.jsx";
 
 import "./i18n.js";
-
 import "./index.css";
 import "./css/designSystem.css";
-import 'bootstrap-icons/font/bootstrap-icons.css'
+import 'bootstrap-icons/font/bootstrap-icons.css';
 
+const Loader = () => {
+  const { t } = useTranslation("general");
+  return (
+    <div className="text-white p-10 text-center">
+      {t("loading", "Loading...")}
+    </div>
+  );
+};
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <Suspense fallback={<Loader />}>
+      <App />
+    </Suspense>
   </StrictMode>
 );
