2a802179f314f91d5c34597a279557940876be4c
hyunjiikim <hjkim96727@gmail.com>
2025-06-23 09:34:07 +0200

    Optimize Chronology Design

--- src/components/ChapterContents/Chronology.jsx
+++ src/components/ChapterContents/Chronology.jsx
@@ -30,18 +30,22 @@ export const Chronology = () => {
     return (
         <div id="chronology" className="flex items-center gap-10 w-fit min-h-[400px] py-10">
             <div className="flex items-center justify-center w-5">
-                <span className="uppercase -rotate-90 font-body tracking-widest text-xs text-nowrap">
+                <span className="uppercase -rotate-90 font-body tracking-widest text-2xl text-nowrap">
                     {t("ch1.chronology.title")}
                 </span>
             </div>
             <div className="flex">
                 {Contents.map((item, index) => (
-                    <div id="chronologyContents" key={index} className="flex flex-col text-white">
-                        <h3 className="">{item.year}</h3>
-                        <div id="div" className="bg-primary-1 w-[117px] h-[20px]" />
-                        <h4>{item.title}</h4>
-                        <p className="font-body text-wrap">{item.description}</p>
+                    <div className="relative">
+                        <div id="chronologyContents" key={index} className="flex flex-col gap-4 text-white p-6 max-w-[280px]">
+                            <h3 className="text-4xl">{item.year}</h3>
+                            <div id="div" className="bg-primary-1 w-[117px] h-[20px]" />
+                            <h4 className="text-3xl">{item.title}</h4>
+                            <p className="font-body text-wrap text-2xl text-wrap">{item.description}</p>
+                        </div>
+                        <hr className="border-1 border-white w-full absolute top-22  left-6 -z-1"  />
                     </div>
+
                 ))}
             </div>
         </div>
