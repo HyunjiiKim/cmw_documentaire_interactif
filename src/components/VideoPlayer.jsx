9c6d0369d5b2a8f04271c190938aa7e794a28f6e
Hyunji Kim <hjkim96727@gmail.com>
2025-06-24 20:01:39 +0200

    automatic sub

--- src/components/VideoPlayer.jsx
+++ src/components/VideoPlayer.jsx
@@ -1,8 +1,17 @@
 import { useEffect, useRef } from "react";
 import Player from "@vimeo/player";
 
-export const VimeoPlayer = ({ videoId, onEnded, width="w-full", height="h-auto", portrait=false }) => {
+export const VimeoPlayer = ({ videoId, onEnded, width = "w-full", height = "h-auto", portrait = false }) => {
   const container = useRef(null);
+
+  let detectedLang = localStorage.getItem("i18nextLng");
+  detectedLang = detectedLang.slice(0, 2);
+
+  // if detectedLang is not french nor Korean, subtitle should be in English
+
+  if (detectedLang !== "fr" && detectedLang !== "ko") {
+    detectedLang = "en";
+  }
 
   useEffect(() => {
     const player = new Player(container.current, {
@@ -11,6 +20,7 @@ export const VimeoPlayer = ({ videoId, onEnded, width="w-full", height="h-auto",
       responsive: true,
       portrait: portrait,
       playsinline: true,
+      texttrack: detectedLang,
     });
 
     if (onEnded) player.on("ended", onEnded);
